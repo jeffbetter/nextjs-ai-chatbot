0730d53a30cde4c7abb5245fe72064ec68cea31f
jeffbetter <70957940+jeffbetter@users.noreply.github.com>
2024-11-05 14:21:26 +0800

    Update page.tsx

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,66 +1,25 @@
 'use client';
 
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useActionState, useEffect, useState } from 'react';
-import { toast } from 'sonner';
-
-import { AuthForm } from '@/components/custom/auth-form';
-import { SubmitButton } from '@/components/custom/submit-button';
-
-import { register, RegisterActionState } from '../actions';
+import { useState } from 'react';
 
 export default function Page() {
   const router = useRouter();
 
   const [email, setEmail] = useState('');
-  const [state, formAction] = useActionState<RegisterActionState, FormData>(
-    register,
-    {
-      status: 'idle',
-    }
-  );
 
-  useEffect(() => {
-    if (state.status === 'user_exists') {
-      toast.error('Account already exists');
-    } else if (state.status === 'failed') {
-      toast.error('Failed to create account');
-    } else if (state.status === 'invalid_data') {
-      toast.error('Failed validating your submission!');
-    } else if (state.status === 'success') {
-      toast.success('Account created successfully');
-      router.refresh();
-    }
-  }, [state, router]);
-
-  const handleSubmit = (formData: FormData) => {
+  const handleSubmit = (formData) => {
     setEmail(formData.get('email') as string);
-    formAction(formData);
+    // 此处不做任何操作，仅显示提示信息
+    alert("本站不支持注册");
   };
 
   return (
     <div className="flex h-dvh w-screen items-start pt-12 md:pt-0 md:items-center justify-center bg-background">
       <div className="w-full max-w-md overflow-hidden rounded-2xl gap-12 flex flex-col">
         <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
-          <h3 className="text-xl font-semibold dark:text-zinc-50">Sign Up</h3>
-          <p className="text-sm text-gray-500 dark:text-zinc-400">
-            Create an account with your email and password
-          </p>
+          <h2>本站不支持注册</h2>
         </div>
-        <AuthForm action={handleSubmit} defaultEmail={email}>
-          <SubmitButton>Sign Up</SubmitButton>
-          <p className="text-center text-sm text-gray-600 mt-4 dark:text-zinc-400">
-            {'Already have an account? '}
-            <Link
-              href="/login"
-              className="font-semibold text-gray-800 hover:underline dark:text-zinc-200"
-            >
-              Sign in
-            </Link>
-            {' instead.'}
-          </p>
-        </AuthForm>
       </div>
     </div>
   );
